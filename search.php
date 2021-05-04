82fd5976cf7a5b509c576f9356bbbc24d66f36c9
giuseppe <placidagiuseppe@gmail.com>
2021-05-04 17:27:07 +0200

    bonus completato

--- search.php
+++ search.php
@@ -1,18 +1,22 @@
 <?php
 
     include './_partials/dati.php';
+    if ($_GET['autore']) {
+      $artist = $_GET['autore'];
+      header('Content-Type: application/json');
+      foreach ($database as $key => $value) {
+          if ($artist == $value['author']) {
+            $db_scelta[] = $value ;
+          }
 
-    $artist = $_GET['autore'];
-    header('Content-Type: application/json');
-    foreach ($database as $key => $value) {
-        if ($artist == $value['author']) {
-          $db_scelta[] = $value ;
-        }
+      }
 
+      // var_dump($db_scelta);
+
+      echo json_encode($db_scelta);
+    }else {
+      echo json_encode($database);
     }
 
-    // var_dump($db_scelta);
-
-    echo json_encode($db_scelta);
 
  ?>
